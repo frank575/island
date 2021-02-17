96926269a134973d68eb8b0b5fb6fa8b9be3c771
吳俊緯 <wcw850710@gmail.com>
2021-02-17 11:40:53 +0800

    init

--- src/api/v1/book.js
+++ src/api/v1/book.js
@@ -1,16 +1,8 @@
 const Router = require('koa-router')
 const router = new Router()
-const { BookValidator } = require('../../validators/validators')
-const { ErrorException } = require('../../core/ErrorException')
 
 router.get('/v1/book/:id', async ctx => {
-  const v = new BookValidator(ctx)
-  const aaa = v.get('query.name', false)
-  await v.validate()
-  ctx.body = {
-    id: v.get('params.id'),
-    name: aaa,
-  }
+  return 'book'
 })
 
 module.exports = router
