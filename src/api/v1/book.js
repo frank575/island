acd4ce97f87f403a347f90bd03e66d5f60a1e59c
吳俊緯 <wcw850710@gmail.com>
2021-02-17 22:06:16 +0800

    docs(book): 測試代碼

--- src/api/v1/book.js
+++ src/api/v1/book.js
@@ -1,8 +1,22 @@
 const Router = require('koa-router')
+const { WeiValidator } = require('../../core/WeiValidator')
 const router = new Router()
 
 router.get('/v1/book/:id', async ctx => {
-  return 'book'
+  const v = await new class extends WeiValidator {
+    constructor (ctx) {
+      super(ctx)
+      this.createRules(r => ({
+        query: {
+          t: r('boolean')
+        },
+        params: {
+          id: r('int', {min: 10})
+        }
+      }))
+    }
+  }(ctx).validate()
+  // return 'book'
 })
 
 module.exports = router
